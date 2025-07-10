89cf5b5f4990a8e95c86b21488cd6bb57265f4a7
marcvr <marc.vanriet@dekimo.com>
2025-07-10 10:51:46 +0200

    wrapper init for when using the repo directly checked out

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""InstrumentLab - controlling test instruments like power supplies and oscilloscopes."""
+
+print("Loading InstrumentLab wrapper __init__...")
+
+import instrumentlab.instrumentlab.siglent as siglent
+import instrumentlab.instrumentlab.keysight as keysight
+import instrumentlab.instrumentlab.tektronix as tektronix
